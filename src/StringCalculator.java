91064492700d3019602f838c4452167431cc28b6
Gouthamamithran Raju Ashok <gouthamamithran.ashok26@outlook.com>
2022-03-22 23:58:38 +0530

    Test case and implementation for Add with comma delimiter
    Added test case and implementation for single number string
    Added test case and implementation for number string with comma delimiter

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,13 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
     public static int add(String numbers) {
+        List<Integer> numberList = new ArrayList<Integer>();
+        int sum = 0;
         if (numbers.isEmpty()) {
             return 0;
         }
-        return 1;
+        String[] numberStrings = numbers.split(",");
+        for (int i = 0; i < numberStrings.length; i++) {
+            sum += Integer.parseInt(numberStrings[i]);
+        }
+
+        return sum;
     }
 
     public static void main(String[] args) {
         System.out.println(StringCalculator.add(""));
+        System.out.println(StringCalculator.add("1"));
+        System.out.println(StringCalculator.add("1,2"));
     }
 
 
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -5,8 +5,20 @@ import static org.junit.Assert.assertEquals;
 public class StringCalculatorTest {
 
     @Test
-    public void testAddWithEmptyString(){
+    public void testAddWithEmptyString() {
         int result = StringCalculator.add("");
         assertEquals(0, result);
+    }
+
+    @Test
+    public void testAddForSingleNumberString() {
+        int result = StringCalculator.add("1");
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testAddForNumberStingsWithComma() {
+        int result = StringCalculator.add("1,2");
+        assertEquals(3, result);
     }
 }
